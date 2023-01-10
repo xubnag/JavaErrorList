d7159a798de0155ed5e51e5d5ef6c6ce6a6f1985
xubang <3307629938@qq.com>
2023-01-10 22:41:02 +0800

    Initial commit

---
+++ src/main/java/com/bangbang/javaerrorlist/JavaErrorListApplication.java
@@ -0,0 +1,13 @@
+package com.bangbang.javaerrorlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaErrorListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaErrorListApplication.class, args);
+    }
+
+}
